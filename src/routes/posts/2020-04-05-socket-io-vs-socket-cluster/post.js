612bc158cdf545b2e1c6ebab4eb051a8c64f725a
Kalob Porter <kalob@kalob.net>
2020-04-05 17:00:35 -0400

    Added socket-io vs socket-cluster

---
+++ src/routes/posts/2020-04-05-socket-io-vs-socket-cluster/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Socket.IO vs Socket Cluster`,
+  tags: ['dev ops', 'frontend', 'backend', 'web sockets'],
+  spoiler: "A high level comparison of Socket.io vs Socket Cluster",
+  getContent: () => import('./document.mdx'),
+}
