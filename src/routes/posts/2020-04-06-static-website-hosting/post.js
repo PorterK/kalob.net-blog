694e8a86e137d06c538440a10770edec9d07aeaa
Kalob Porter <kalob@kalob.net>
2020-04-06 20:12:31 -0400

    Added static website hosting blog post

---
+++ src/routes/posts/2020-04-06-static-website-hosting/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Near free React hosting on AWS`,
+  tags: ['dev ops', 'frontend', 'react'],
+  spoiler: "How to host your React app on AWS",
+  getContent: () => import('./document.mdx'),
+}
