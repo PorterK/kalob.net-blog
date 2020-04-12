117f29aaed481b5692c4cf654dd73946fb8de015
Kalob Porter <kalob@kalob.net>
2020-04-11 21:12:33 -0400

    Change tag name

--- src/routes/posts/2020-04-11-ci-cd-jenkins/post.js
+++ src/routes/posts/2020-04-11-ci-cd-jenkins/post.js
@@ -2,7 +2,7 @@ import TwitterCard from './twitter-card.png';
 
 export default {
   title: `Automating Deployment with Travis CI`,
-  tags: ['dev ops', 'ci/cd'],
+  tags: ['dev ops', 'ci', 'cd'],
   spoiler: "Automating deployments on Github with Travis CI",
   cardImage: TwitterCard,
   getContent: () => import('./document.mdx'),
